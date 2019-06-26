b162aefe1aee3f3407d00af8dce75321e07d9ea1
Samuel Brehm <samuelbrehm@gmail.com>
2019-06-26 11:01:41 -0300

    Initial settings

---
+++ src/pages/Main/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Main = () => <View style={{ backgroundColor: '##FFF', flex: 1 }} />;
+
+export default Main;
