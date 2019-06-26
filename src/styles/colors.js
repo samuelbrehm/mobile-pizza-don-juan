981b55d5d1f97d80da44050fe60f4deb49ab5f0b
Samuel Brehm <samuelbrehm@gmail.com>
2019-06-26 15:09:10 -0300

    Settings global styles

---
+++ src/styles/colors.js
@@ -0,0 +1,13 @@
+export default {
+  background: '#F4F4F4',
+
+  primary: '#e5293e',
+  seundary: '#0b2031',
+
+  white: '#FFF',
+  lighter: '#B9BBBE',
+  light: '#999',
+  grey: '#706e7b',
+
+  inputBackground: 'rgba(255, 255, 255)',
+};
---
+++ src/styles/index.js
@@ -0,0 +1,4 @@
+import colors from './colors';
+import * as general from './general';
+
+export { colors, general };
